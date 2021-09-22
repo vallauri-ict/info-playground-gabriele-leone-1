188a2b64947b4687f9759e98b61b396db1344c95
Gabriele <62541822+gabriele-leone@users.noreply.github.com>
2021-09-22 17:51:57 +0200

    Esercizio 2 - Realizzare progetto visual studio con DB

---
+++ Ese02_Aereoporto/SQLQuery1.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE Aereoporto
+(
+	citta VARCHAR(50) NOT NULL PRIMARY KEY,
+	nazione VARCHAR(3) NOT NULL,
+	numPiste INT NOT NULL
+)
+
+CREATE TABLE Volo
+(
+	idVolo INT NOT NULL PRIMARY KEY IDENTITY (1, 1),
+	giornoSettimana SMALLINT NOT NULL,
+	cittaPartenza VARCHAR(50) NOT NULL,
+	oraPartenza TIME NOT NULL,
+	cittaArrivo VARCHAR(50) NOT NULL,
+	oraArrivo TIME NOT NULL,
+	tipoAereo VARCHAR(30) NOT NULL
+)
+
+CREATE TABLE Aereo
+(
+	tipoAereo VARCHAR(30) NOT NULL PRIMARY KEY,
+	numPasseggeri INT NOT NULL,
+	qtaMerci INT NOT NULL
+)
