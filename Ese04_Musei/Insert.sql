4ad3f9f8800061111ea2944dcc96338f9f574635
Gabriele <62541822+gabriele-leone@users.noreply.github.com>
2021-10-01 11:05:36 +0200

    Update

---
+++ Ese04_Musei/Insert.sql
@@ -0,0 +1,19 @@
+﻿INSERT INTO Museo VALUES ('National Gallery', 'Londra')
+INSERT INTO Museo VALUES ('Galleria degli Uffizi', 'Firenze')
+INSERT INTO Museo VALUES ('British Museum', 'Londra')
+
+INSERT INTO Artista VALUES ('Caravaggio', 'FR')
+INSERT INTO Artista VALUES ('Tiziano', 'IT')
+INSERT INTO Artista VALUES ('Leonardo', 'IT')
+
+INSERT INTO Opera VALUES ('A', 'British Museum', 'Tiziano')
+INSERT INTO Opera VALUES ('B', 'British Museum', 'Tiziano')
+INSERT INTO Opera VALUES ('C', 'National Gallery', 'Leonardo')
+INSERT INTO Opera VALUES ('D', 'Galleria degli Uffizi', 'Caravaggio')
+INSERT INTO Opera VALUES ('E', 'Galleria degli Uffizi', 'Caravaggio')
+
+INSERT INTO Personaggio VALUES ('Madonna', 1)
+INSERT INTO Personaggio VALUES ('Gesù', 1)
+INSERT INTO Personaggio VALUES ('Tizio', 3)
+INSERT INTO Personaggio VALUES ('Caio', 4)
+INSERT INTO Personaggio VALUES ('Sempronio', 5)
---
+++ Ese04_Musei/SQLQuery1.sql
@@ -0,0 +1,9 @@
+﻿/*Il nome dei musei di londra che non conservano opere di tiziano*/
+SELECT m.NomeM
+FROM Museo m
+WHERE m.Citta='Londra'
+AND NOT EXISTS(SELECT * 
+				FROM Opera o
+				WHERE o.NomeA='Tiziano'
+				AND o.NomeM=m.NomeM
+				)
